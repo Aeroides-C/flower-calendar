40cafdaf5cbf8f750a3989041975c6401add5c90
Aeroides-C <1154020818@qq.com>
2026-09-10 16:01:48 +0800

    更改布局 2

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// 默认成熟时间（小时），因为没有下拉菜单了，这里固定一个值或者你可以自己改
-// 比如默认设为 72 小时（3天），你可以根据需要修改这个数字
 const DEFAULT_GROWTH_HOURS = 72; 
 
 let countdownInterval;
@@ -14,10 +12,8 @@ function calculate() {
 
     const plantDate = new Date(plantInput);
     
-    // 计算收获时间 = 种植时间 + 默认小时数
     const harvestDate = new Date(plantDate.getTime() + DEFAULT_GROWTH_HOURS * 60 * 60 * 1000);
 
-    // 格式化日期显示
     const harvestStr = harvestDate.toLocaleString('zh-CN', { 
         year: 'numeric', month: '2-digit', day: '2-digit', 
         hour: '2-digit', minute: '2-digit' 
@@ -25,12 +21,10 @@ function calculate() {
 
     document.getElementById('harvestTime').innerText = harvestStr;
 
-    // 开始倒计时
     startCountdown(harvestDate);
 }
 
 function startCountdown(targetDate) {
-    // 清除之前的定时器
     if (countdownInterval) clearInterval(countdownInterval);
 
     function update() {
@@ -52,6 +46,6 @@ function startCountdown(targetDate) {
             `${days}天 ${hours}时 ${minutes}分 ${seconds}秒`;
     }
 
-    update(); // 立即执行一次
-    countdownInterval = setInterval(update, 1000); // 每秒更新
+    update();
+    countdownInterval = setInterval(update, 1000);
 }
